7493fbf5e6fe001d290683632f7249e0ef237d5a
amael-ls <amael.lesquin@gmail.com>
2024-05-26 22:56:12 +0200

    Generalised the function Extract_num to extract content from commands with more than one argument. Renamed it to Extract_content. Started to code to format the \qty command

--- siunitx-quarto/_extensions/siunitx-quarto/siunitx-quarto.lua
+++ siunitx-quarto/_extensions/siunitx-quarto/siunitx-quarto.lua
@@ -1,19 +1,21 @@
 
--- Extract number from latex num command. s is a string
-function Extract_num(s)
+-- Extract content between {} of Latex command. s is a string.
+local function Extract_content(s, command)
   s = string.gsub(s, " ", "")
-  _, loc_start = string.find(s, "\\num{")
+  _, loc_start = string.find(s, "\\" .. command .. "{")
 
-  if (loc_start ~= nil) then
+  content_cmd = {}
+  while (loc_start ~= nil) do
     _, loc_end = string.find(s, "}", loc_start + 1)
-    num = string.sub(s, loc_start + 1, loc_end - 1)
-    return num
+    content_cmd[#content_cmd + 1] = string.sub(s, loc_start + 1, loc_end - 1) -- append
+    _, loc_start = string.find(s, "{", loc_end + 1)
   end
-  return nil
+
+  return content_cmd
 end
 
 -- Format numbers
-function Format_num(number, sep)
+local function Format_num(number, sep)
   -- Split the sign, the significand, and the fractional part
   local _, _, minus, int, fraction = tostring(number):find('([-]?)(%d+)([.]?%d*)')
 
@@ -24,53 +26,25 @@ function Format_num(number, sep)
   return minus .. int:reverse():gsub("^" .. sep, "") .. fraction
 end
 
---! THIS PARA FILTER MIGHT STILL HAVE BUGS
--- Filter to detect if there are spaces after '{' or before '}' in \num{ xyz } which would create problems
--- Para = function(element)
---   -- Function to process \num{...} and remove spaces within the braces, with anonymous function to remove spaces from the content within braces
---   local function process_latex_num(text)
---     return text:gsub("\\num%s*{([^}]*)}", function(num_content)
---       local trimmed_content = num_content:gsub("%s+", "")
---       return "\\num{" .. trimmed_content .. "}"
---     end)
---   end
-
---   -- Rebuild sentences from Para, keeping only words and spaces
---   local concatenate_str = ""
---   for _, inline in ipairs(element) do
---     if inline.t == "Str" then
---       concatenate_str = concatenate_str .. inline
---     elseif inline.t == "Space" then
---       concatenate_str = concatenate_str .. " "
---     end -- Do nothing for the other types that can be met in Para
---   end
-  
---   -- Remove spurious spaces
---   concatenate_str = process_latex_num(concatenate_str)
-
---   -- Rebuild the Para object with the corrected \\num latex commands
---   local original_para = {}
---   for word in concatenate_str:gmatch("%S+") do
---     table.insert(original_para, pandoc.Str(word))
---     table.insert(original_para, pandoc.Space())
---   end
-  
---   -- Remove the last added Space if present (by first checking that original_para is not empty)
---   if #original_para ~= 0 and original_para[#original_para].t == "Space" then
---     table.remove(original_para, #original_para)
---   end
-
---   -- Return the modified paragraph
---   element.content = original_para
---   return element
--- end
+-- Format units
+local function Format_units(s)
+  print("print from Format_units = " .. s)
+  return "lala"
+end
 
 -- Filter to modify the output 
 RawInline = function(element)
   if FORMAT:match 'html' then
+    -- Handling the \num command
     pos, _ = string.find(element.text, "\\num{")
     if pos ~= nil then
-      return pandoc.Str(Format_num(Extract_num(element.text), " "))
+      return pandoc.Str(Format_num(Extract_content(element.text, "num")[1], " "))
+    end
+
+  --   -- Handling the \qty{}{} command, the same should apply to \SI and \si commands although outdated
+    pos, _ = string.find(element.text, "\\qty{")
+    if pos ~= nil then
+      return pandoc.Str(Format_units(Extract_content(element.text, "qty")[1])) -- ! RESTART HERE. pandoc.Para ???
     end
   end
   return element
